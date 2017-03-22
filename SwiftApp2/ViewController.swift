ee0272a39a87f5e838ded3e98e87b399aacfd91c
Cibele Bustamante <cibelebustamante@gmail.com>
2017-03-22 13:44:34 -0300

    one

--- SwiftApp2/ViewController.swift
+++ SwiftApp2/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        
+         theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
