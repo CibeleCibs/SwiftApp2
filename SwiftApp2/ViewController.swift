8e6606dbdbb09213fd452e5fa4155100596a27d3
Cibele Bustamante <cibelebustamante@gmail.com>
2017-03-22 16:35:24 -0300

    third

--- SwiftApp2/ViewController.swift
+++ SwiftApp2/ViewController.swift
@@ -20,19 +20,30 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTap(_ sender: Any) {
         
-         theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        var addition = true
+        
+        if addition {
+            
+            theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        } else {
+            theLabel.text = "\(Double(text1.text!)! - Double(text2.text!)!)"
+            
+        }
+        
+        
+        
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
